79a48e413ba1a14aca388a9b4577157581091e1c
Laoluafolami <laoluafolami@outlook.com>
2025-04-09 18:03:20 +0100

    Modified code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('This is the world of CI/CD. You have not seen anything yet. We will be show casing how the delivery pipeline work whenever you push and commit your code repositry in github. You just seat back and enjoy the magic'));
+app.get('/', (req, res) => res.send(' You have not seen anything yet. We will be show casing how the delivery pipeline work whenever you push and commit your code repositry in github. You just seat back and enjoy the magic'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
